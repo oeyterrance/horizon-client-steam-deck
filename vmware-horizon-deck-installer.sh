789c96f73468b731a165b4623bd13def16bf39c9
oeyterrance <103081472+oeyterrance@users.noreply.github.com>
2024-01-11 18:33:14 -0500

    Update vmware-horizon-deck-installer.sh
    Update to latest vmware

--- vmware-horizon-deck-installer.sh
+++ vmware-horizon-deck-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd $HOME/Downloads
 rm -rf VMware-Horizon-Client*
-wget https://download3.vmware.com/software/CART23FQ3_LIN64_2209/VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle
-chmod +x VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle
+wget https://download3.vmware.com/software/CART24FQ4_LIN64_2309.1/VMware-Horizon-Client-2309.1-8.11.1-22775487.x64.bundle
+chmod +x VMware-Horizon-Client-2309.1-8.11.1-22775487.x64.bundle
 sudo steamos-readonly disable
 sudo rm -rf /etc/vmware*
 sudo rm -rf $HOME/.vmware/*
 sudo rmdir $HOME/.vmware
-sudo ./VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle
+sudo ./VMware-Horizon-Client-2309.1-8.11.1-22775487.x64.bundle
 THEME=`kreadconfig5 --key LookAndFeelPackage`
 sudo sed -i "s/Exec=vmware-view %u/Exec=env GTK_THEME=$THEME vmware-view %u/g" /usr/share/applications/vmware-view.desktop
 sudo steamos-readonly enable
